aada2ecf973b0d33b28d4585e1a3793a9aadc010
Patrick Boucher <pboucher@patrickboucher.com>
2013-02-25 20:12:29 -0500

    #20039 - Move functions to instance methods and use instance data

--- app.py
+++ app.py
@@ -13,6 +13,13 @@ import tank
 
 class LaunchApplication(tank.platform.Application):
     """Mutli App to launch applications."""
+    def __init__(self, *args, **kwargs):
+        super(LaunchApplication, self).__init__(*args, **kwargs)
+        self._app_path = None           # Path to the application executable
+        self._app_args = None           # Executable arguments (str)
+        self._engine_path = None        # Path to the engine to use
+        self._extra_configs = {}        # Extra per engine
+        self._system = sys.platform     # Platform this app is running on (should be in tank.platform.Application?)
 
     def init_app(self):
         menu_name = self.get_setting("menu_name")
@@ -38,7 +45,7 @@ class LaunchApplication(tank.platform.Application):
         entity_id = entity_ids[0]
         context = self.tank.context_from_entity(entity_type, entity_id)
         engine_name = self.get_setting("engine")
-        engine_path = tank.platform.get_engine_path(engine_name, self.tank, context)
+        self._engine_path = tank.platform.get_engine_path(engine_name, self.tank, context)
 
         # Try to create path for the context.
         try:
@@ -47,14 +54,13 @@ class LaunchApplication(tank.platform.Application):
             raise Exception("Could not create folders on disk. Error reported: %s" % e)            
 
         # get the setting
-        system = sys.platform
         try:
-            system_name = {"linux2": "linux", "darwin": "mac", "win32": "windows"}[system]
-            app_path = self.get_setting("%s_path" % system_name, "")
-            app_args = self.get_setting("%s_args" % system_name, "")
-            if not app_path: raise KeyError()
+            system_name = {"linux2": "linux", "darwin": "mac", "win32": "windows"}[self._system]
+            self._app_path = self.get_setting("%s_path" % system_name, "")
+            self._app_args = self.get_setting("%s_args" % system_name, "")
+            if not self._app_path: raise KeyError()
         except KeyError:
-            raise Exception("Platform '%s' is not supported." % system)
+            raise Exception("Platform '%s' is not supported." % self._system)
 
         # Set environment variables used by apps to prep Tank engine
         os.environ["TANK_ENGINE"] = engine_name
@@ -63,23 +69,23 @@ class LaunchApplication(tank.platform.Application):
         os.environ["TANK_ENTITY_ID"] = str(entity_id)
 
         # Prep any application specific things
-        extra_configs = self.get_setting("extra", {})
+        self._extra_configs = self.get_setting("extra", {})
         if engine_name == "tk-nuke":
-            _tk_nuke()
+            self.prepare_nuke_launch()
         elif engine_name == "tk-maya":
-            _tk_maya(system, app_path)
+            self.prepare_maya_launch()
         elif engine_name == "tk-motionbuilder":
-            app_args = _tk_motionbuilder(app_args)
+            self.prepare_motionbuilder_launch()
         elif engine_name == "tk-3dsmax":
-            app_args = _tk_3dsmax(app_args)
+            self.prepare_3dsmax_launch()
         elif engine_name == "tk-hiero":
-            _tk_hiero()
+            self.prepare_hiero_launch()
         elif engine_name == "tk-photoshop":
-            _tk_photoshop(extra_configs, engine_path)
+            self.prepare_photoshop_launch()
 
         # Launch the application
-        self.log_debug("Launching executable '%s' with args '%s'" % (app_path, app_args))
-        result = self.execute_hook("hook_app_launch", app_path=app_path, app_args=app_args)
+        self.log_debug("Launching executable '%s' with args '%s'" % (self._app_path, self._app_args))
+        result = self.execute_hook("hook_app_launch", app_path=self._app_path, app_args=self._app_args)
         cmd = result.get("command")
         launch_error = result.get("launch_error")
 
@@ -115,129 +121,131 @@ class LaunchApplication(tank.platform.Application):
         tank.util.create_event_log_entry(self.tank, ctx, "Tank_App_Startup", desc, meta)
 
 
-def _tk_nuke():
-    """Nuke specific pre-launch environment setup."""
+    def prepare_nuke_launch(self):
+        """Nuke specific pre-launch environment setup."""
 
-    # Make sure Nuke can find the Tank menu
-    startup_path = os.path.abspath(os.path.join(_get_app_specific_path("nuke"), "startup"))
-    tank.util.append_path_to_env_var("NUKE_PATH", startup_path)
-
-
-def _tk_maya(system, app_path):
-    """Maya specific pre-launch environment setup."""
-
-    # Make sure Maya can find the Tank menu
-    app_specific_path = _get_app_specific_path("maya")
-    startup_path = os.path.abspath(os.path.join(app_specific_path, "startup"))
-    tank.util.append_path_to_env_var("PYTHONPATH", startup_path)
-
-    # Push our patched _ssl compiled module to the front of the PYTHONPATH for Windows
-    # SSL Connection time fix.
-    if system == "win32":
-        # maps the maya version to the ssl maya version;  (maya 2011 can use the maya 2012 _ssl.pyd)
-        # the ssl directory name is the version of maya it was compiled for.
-        maya_version_to_ssl_maya_version = {
-            "2011": "2012",
-            "2012": "2012",
-            "2013": "2013",
-        }
-
-        version_dir = None
-        # From most recent to past version
-        for year in sorted(maya_version_to_ssl_maya_version, reverse=True):
-            # Test for the year in the path.
-            # maya -v returns an empty line with maya 2013.
-            if year in app_path:
-                version_dir = maya_version_to_ssl_maya_version[year]
-                break
-
-        # if there is an ssl lib for that current version of maya being used then
-        # add it to the python path.
-        if version_dir:
-            ssl_path = os.path.abspath(os.path.join(app_specific_path, "ssl_patch", version_dir))
-            tank.util.prepend_path_to_env_var("PYTHONPATH", ssl_path)
+        # Make sure Nuke can find the Tank menu
+        startup_path = os.path.abspath(os.path.join(self._get_app_specific_path("nuke"), "startup"))
+        tank.util.append_path_to_env_var("NUKE_PATH", startup_path)
 
 
-def _tk_motionbuilder(app_args):
-    """Maya specific pre-launch environment setup."""
+    def prepare_maya_launch(self):
+        """Maya specific pre-launch environment setup."""
 
-    if app_args:
-        app_args += " "
-    return app_args + "\"%s\"" % os.path.join(_get_app_specific_path("motionbuilder"), "startup", "init_tank.py")
+        # Make sure Maya can find the Tank menu
+        app_specific_path = self._get_app_specific_path("maya")
+        startup_path = os.path.abspath(os.path.join(app_specific_path, "startup"))
+        tank.util.append_path_to_env_var("PYTHONPATH", startup_path)
 
+        # Push our patched _ssl compiled module to the front of the PYTHONPATH for Windows
+        # SSL Connection time fix.
+        if self._system == "win32":
+            # maps the maya version to the ssl maya version;  (maya 2011 can use the maya 2012 _ssl.pyd)
+            # the ssl directory name is the version of maya it was compiled for.
+            maya_version_to_ssl_maya_version = {
+                "2011": "2012",
+                "2012": "2012",
+                "2013": "2013",
+            }
 
-def _tk_3dsmax(app_args):
-    """
-    3DSMax specific pre-launch environment setup.
+            version_dir = None
+            # From most recent to past version
+            for year in sorted(maya_version_to_ssl_maya_version, reverse=True):
+                # Test for the year in the path.
+                # maya -v returns an empty line with maya 2013.
+                if year in self._app_path:
+                    version_dir = maya_version_to_ssl_maya_version[year]
+                    break
 
-    Make sure launch args include a maxscript to load the python engine:
-    3dsmax.exe somefile.max -U MAXScript somescript.ms        
-    """
-
-    startup_dir = os.path.abspath(os.path.join(_get_app_specific_path("3dsmax"), "startup"))
-    os.environ["TANK_BOOTSTRAP_SCRIPT"] = os.path.join(startup_dir, "tank_startup.py")
-    new_args = "-U MAXScript \"%s\"" % os.path.join(startup_dir, "init_tank.ms")
-
-    if app_args:
-        app_args = " " + app_args
-    return new_args + app_args
-
-
-def _tk_hiero():
-    """Hiero specific pre-launch environment setup."""
-
-    startup_path = os.path.abspath(os.path.join(_get_app_specific_path("hiero"), "startup"))
-    tank.util.append_path_to_env_var("HIERO_PLUGIN_PATH", startup_path)
-
-
-def _tk_photoshop(extra_config, engine_path):
-    """Photoshop specific pre-launch environment setup."""
-
-    if engine_path is None:
-        raise ValueError("Path to photoshop engine (tk-photoshop) could not be found.")
-
-    # Get the path to the python executable
-    system = sys.platform
-    try:
-        python_setting = {"darwin": "mac_python_path", "win32": "windows_python_path"}[system]
-    except KeyError:
-        raise Exception("Platform '%s' is not supported." % system)
-    python_path = extra_config.get(python_setting)
-    if not python_path:
-        raise Exception("Missing extra setting %s" % python_setting)
-
-    # get the path to extension manager
-    try:
-        manager_setting = {
-            "darwin": "mac_extension_manager_path",
-            "win32": "windows_extension_manager_path"
-        }[system]
-    except KeyError:
-        raise Exception("Platform '%s' is not supported." % system)
-    manager_path = extra_config.get(manager_setting)
-    if not manager_path:
-        raise Exception("Missing extra setting %s" % manager_setting)
-    os.environ["TANK_PHOTOSHOP_EXTENSION_MANAGER"] = manager_path
-
-    # make sure the extension is up to date
-    sys.path.append(os.path.join(engine_path, "bootstrap"))
-    import photoshop_extension_manager
-    photoshop_extension_manager.update()
-
-    # Store data needed for bootstrapping Tank in env vars. Used in startup/menu.py
-    os.environ["TANK_PHOTOSHOP_PYTHON"] = python_path
-    os.environ["TANK_PHOTOSHOP_BOOTSTRAP"] = os.path.join(engine_path, "bootstrap", "engine_bootstrap.py")
-    os.environ["TANK_PHOTOSHOP_ENGINE"] = os.environ["TANK_ENGINE"]
-    os.environ["TANK_PHOTOSHOP_PROJECT_ROOT"] = os.environ["TANK_PROJECT_ROOT"]
-    os.environ["TANK_PHOTOSHOP_ENTITY_TYPE"] = os.environ["TANK_ENTITY_TYPE"]
-    os.environ["TANK_PHOTOSHOP_ENTITY_ID"] = os.environ["TANK_ENTITY_ID"]
-
-    # add our startup path to the photoshop init path
-    startup_path = os.path.abspath(os.path.join(_get_app_specific_path("photoshop"), "startup"))
-    tank.util.append_path_to_env_var("PYTHONPATH", startup_path)
+            # if there is an ssl lib for that current version of maya being used then
+            # add it to the python path.
+            if version_dir:
+                ssl_path = os.path.abspath(os.path.join(app_specific_path, "ssl_patch", version_dir))
+                tank.util.prepend_path_to_env_var("PYTHONPATH", ssl_path)
 
 
-def _get_app_specific_path(app_dir):
-    """Get the path for application specific files for a given application."""
+    def prepare_motionbuilder_launch(self):
+        """Maya specific pre-launch environment setup."""
 
-    return os.path.join(os.path.dirname(__file__), "app_specific", app_dir)
+        new_args = "\"%s\"" % os.path.join(self._get_app_specific_path("motionbuilder"), "startup", "init_tank.py")
+
+        if self._app_args:
+            self._app_args += " "
+        self._app_args += new_args
+
+
+    def prepare_3dsmax_launch(self):
+        """
+        3DSMax specific pre-launch environment setup.
+
+        Make sure launch args include a maxscript to load the python engine:
+        3dsmax.exe somefile.max -U MAXScript somescript.ms        
+        """
+
+        startup_dir = os.path.abspath(os.path.join(self._get_app_specific_path("3dsmax"), "startup"))
+        os.environ["TANK_BOOTSTRAP_SCRIPT"] = os.path.join(startup_dir, "tank_startup.py")
+        new_args = "-U MAXScript \"%s\"" % os.path.join(startup_dir, "init_tank.ms")
+
+        if self._app_args:
+            self._app_args = " " + self._app_args
+        return new_args + self._app_args
+
+
+    def prepare_hiero_launch(self):
+        """Hiero specific pre-launch environment setup."""
+
+        startup_path = os.path.abspath(os.path.join(self._get_app_specific_path("hiero"), "startup"))
+        tank.util.append_path_to_env_var("HIERO_PLUGIN_PATH", startup_path)
+
+
+    def prepare_photoshop_launch(self):
+        """Photoshop specific pre-launch environment setup."""
+
+        if self._engine_path is None:
+            raise ValueError("Path to photoshop engine (tk-photoshop) could not be found.")
+
+        # Get the path to the python executable
+        system = sys.platform
+        try:
+            python_setting = {"darwin": "mac_python_path", "win32": "windows_python_path"}[system]
+        except KeyError:
+            raise Exception("Platform '%s' is not supported." % system)
+        python_path = self._extra_configs.get(python_setting)
+        if not python_path:
+            raise Exception("Missing extra setting %s" % python_setting)
+
+        # get the path to extension manager
+        try:
+            manager_setting = {
+                "darwin": "mac_extension_manager_path",
+                "win32": "windows_extension_manager_path"
+            }[system]
+        except KeyError:
+            raise Exception("Platform '%s' is not supported." % system)
+        manager_path = self._extra_configs.get(manager_setting)
+        if not manager_path:
+            raise Exception("Missing extra setting %s" % manager_setting)
+        os.environ["TANK_PHOTOSHOP_EXTENSION_MANAGER"] = manager_path
+
+        # make sure the extension is up to date
+        sys.path.append(os.path.join(self._engine_path, "bootstrap"))
+        import photoshop_extension_manager
+        photoshop_extension_manager.update()
+
+        # Store data needed for bootstrapping Tank in env vars. Used in startup/menu.py
+        os.environ["TANK_PHOTOSHOP_PYTHON"] = python_path
+        os.environ["TANK_PHOTOSHOP_BOOTSTRAP"] = os.path.join(self._engine_path, "bootstrap", "engine_bootstrap.py")
+        os.environ["TANK_PHOTOSHOP_ENGINE"] = os.environ["TANK_ENGINE"]
+        os.environ["TANK_PHOTOSHOP_PROJECT_ROOT"] = os.environ["TANK_PROJECT_ROOT"]
+        os.environ["TANK_PHOTOSHOP_ENTITY_TYPE"] = os.environ["TANK_ENTITY_TYPE"]
+        os.environ["TANK_PHOTOSHOP_ENTITY_ID"] = os.environ["TANK_ENTITY_ID"]
+
+        # add our startup path to the photoshop init path
+        startup_path = os.path.abspath(os.path.join(self._get_app_specific_path("photoshop"), "startup"))
+        tank.util.append_path_to_env_var("PYTHONPATH", startup_path)
+
+
+    def _get_app_specific_path(self, app_dir):
+        """Get the path for application specific files for a given application."""
+
+        return os.path.join(os.path.dirname(__file__), "app_specific", app_dir)
