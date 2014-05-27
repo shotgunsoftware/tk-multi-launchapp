d9ea7b96410e6967e63de7b40fd3d3fcf27b6b20
Manne Öhrström <manne@shotgunsoftware.com>
2014-05-27 17:05:02 +0200

    [minor] Now external engines handle deferred folders correctly.

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 # Copyright (c) 2013 Shotgun Software Inc.
-# 
+#
 # CONFIDENTIAL AND PROPRIETARY
-# 
-# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit 
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
 # Source Code License included in this distribution package. See LICENSE.
-# By accessing, using, copying or modifying this work you indicate your 
-# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
 """
@@ -91,13 +91,13 @@ class LaunchApplication(tank.platform.Application):
         if command_name.endswith("..."):
             command_name = command_name[:-3]
 
-        # special case! todo: fix this. 
+        # special case! todo: fix this.
         # this is to allow this app to be loaded for sg entities of type publish
         # but not show up on the menu.
         # this is because typically, for published files, and versions, you want the app loaded
         # but you want to access it via the launch_from_path() method, normally
         # hooked up via a hook.
-        if self.engine.environment.get("name") not in ["shotgun_tankpublishedfile", 
+        if self.engine.environment.get("name") not in ["shotgun_tankpublishedfile",
                                                        "shotgun_publishedfile",
                                                        "shotgun_version"]:
 
@@ -106,24 +106,24 @@ class LaunchApplication(tank.platform.Application):
                            "description": "Launches and initializes an application environment.",
                            "icon": icon,
                          }
-                
+
             def launch_version():
                 self.launch_from_entity(version)
             self.engine.register_command(command_name, launch_version, properties)
-        
-        
+
+
     def launch_from_path_and_context(self, path, context, version=None):
         """
-        Extended version of launch_from_path. This method takes an additional 
+        Extended version of launch_from_path. This method takes an additional
         context parameter which is useful if you want to seed the launch context
         with more context data than is available in the path itself. Typically
         paths may not contain a task, so this may need to be pushed through
         separately via the context.
-        
+
         Entry point if you want to launch an app given a particular path.
         Note that there are no checks that the path passed is actually compatible
-        with the app that is being launched. This should be handled in logic 
-        which is external to this app. 
+        with the app that is being launched. This should be handled in logic
+        which is external to this app.
         """
         if context is None:
             # this context looks sour. So fall back on to path-only launch.
@@ -137,8 +137,8 @@ class LaunchApplication(tank.platform.Application):
         """
         Entry point if you want to launch an app given a particular path.
         Note that there are no checks that the path passed is actually compatible
-        with the app that is being launched. This should be handled in logic 
-        which is external to this app. 
+        with the app that is being launched. This should be handled in logic
+        which is external to this app.
         """
         context = self.tank.context_from_path(path)
         self._launch_app(context, path, version=version)
@@ -151,13 +151,13 @@ class LaunchApplication(tank.platform.Application):
         # extract a entity_type and id from the context.
         if self.context.project is None:
             raise TankError("Your context does not have a project defined. Cannot continue.")
-        
+
         # make sure that we don't launch from tasks with no step - while this is not technically
-        # incorrect, it can be confusing with any config that requires a step. 
+        # incorrect, it can be confusing with any config that requires a step.
         if self.engine.name == "tk-shotgun" and self.context.task and self.context.step is None:
             raise TankError("Looks like you are trying to launch from a Task that "
-                            "does not have a Pipeline Step associated! ")  
-        
+                            "does not have a Pipeline Step associated! ")
+
         # first do project
         entity_type = self.context.project["type"]
         entity_id = self.context.project["id"]
@@ -169,25 +169,33 @@ class LaunchApplication(tank.platform.Application):
         if self.context.task:
             entity_type = self.context.task["type"]
             entity_id = self.context.task["id"]
-        
+
         # Try to create path for the context.
         engine_name = self.get_setting("engine")
-        
+
         if engine_name:
             # we are starting an engine, so do deferred folder creation
             try:
                 self.log_debug("Creating folders for %s %s, %s" % (entity_type, entity_id, engine_name))
                 self.tank.create_filesystem_structure(entity_type, entity_id, engine=engine_name)
             except tank.TankError, err:
-                raise TankError("Could not create folders on disk. Error reported: %s" % err)            
+                raise TankError("Could not create folders on disk. Error reported: %s" % err)
 
         else:
             # we are not starting and engine, so do normal folder creation
+            #
+            # Note: We pass the menu name instead of the engine name (which is None) into
+            # the folder creation. This will ensure that secondary folder creation passes
+            # (eg. like user sandboxes) are triggered and also makes it possible to actually
+            # set up spefific deferred folder creation for external engines by using the
+            # menu name in the config.
+            menu_name = self.get_setting("menu_name")
+
             try:
                 self.log_debug("Creating folders for %s %s" % (entity_type, entity_id))
-                self.tank.create_filesystem_structure(entity_type, entity_id)
+                self.tank.create_filesystem_structure(entity_type, entity_id, engine=menu_name)
             except tank.TankError, err:
-                raise TankError("Could not create folders on disk. Error reported: %s" % err)            
+                raise TankError("Could not create folders on disk. Error reported: %s" % err)
 
         self._launch_app(self.context, version=version)
 
@@ -221,20 +229,20 @@ class LaunchApplication(tank.platform.Application):
 
         engine_name = self.get_setting("engine")
         if engine_name:
-            
+
             # we have an engine we should start as part of this app launch
             # pass down the file to open into the startup script via env var.
             if file_to_open:
                 os.environ["TANK_FILE_TO_OPEN"] = file_to_open
                 self.log_debug("Setting TANK_FILE_TO_OPEN to '%s'" % file_to_open)
-                
+
             # serialize the context into an env var
             os.environ["TANK_CONTEXT"] = tank.context.serialize(context)
             self.log_debug("Setting TANK_CONTEXT to '%r'" % context)
-    
+
             # Set environment variables used by apps to prep Tank engine
             os.environ["TANK_ENGINE"] = engine_name
-                
+
             # Prep any application specific things
             if engine_name == "tk-nuke":
                 app_args = self.prepare_nuke_launch(file_to_open, app_args)
@@ -264,7 +272,7 @@ class LaunchApplication(tank.platform.Application):
         # Launch the application
         self.log_debug("Launching executable '%s' with args '%s'" % (app_path, app_args))
         result = self.execute_hook("hook_app_launch", app_path=app_path, app_args=app_args)
-        
+
         cmd = result.get("command")
         return_code = result.get("return_code")
 
@@ -278,7 +286,7 @@ class LaunchApplication(tank.platform.Application):
                 "questions, don't hesitate to contact support on toolkitsupport@shotgunsoftware.com." %
                 (return_code, cmd)
             )
-            
+
         else:
             # Write an event log entry
             self._register_event_log(context, cmd, version)
@@ -294,8 +302,8 @@ class LaunchApplication(tank.platform.Application):
 
         meta = {}
         meta["core"] = self.tank.version
-        meta["engine"] = "%s %s" % (self.engine.name, self.engine.version) 
-        meta["app"] = "%s %s" % (self.name, self.version) 
+        meta["engine"] = "%s %s" % (self.engine.name, self.engine.version)
+        meta["app"] = "%s %s" % (self.name, self.version)
         meta["launched_engine"] = self.get_setting("engine")
         meta["command"] = command_executed
         meta["platform"] = sys.platform
@@ -312,7 +320,7 @@ class LaunchApplication(tank.platform.Application):
         # Make sure Nuke can find the Tank menu
         startup_path = os.path.abspath(os.path.join(self._get_app_specific_path("nuke"), "startup"))
         tank.util.append_path_to_env_var("NUKE_PATH", startup_path)
-        
+
         # it's not possible to open a nuke script from within the initialization
         # scripts so if we have a path then we need to pass it through the start
         # up args:
@@ -367,13 +375,13 @@ class LaunchApplication(tank.platform.Application):
         # add the startup plug-in to the XSI_PLUGINS path:
         xsi_plugins = os.path.abspath(os.path.join(self._get_app_specific_path("softimage"), "startup", "Application", "Plugins"))
         tank.util.append_path_to_env_var("XSI_PLUGINS", xsi_plugins)
-        
-        # On Linux, Softimage 2013 is missing libssl and sqlite3 libraries.  We have some that 
-        # we think will work so lets _append_ them to the LD_LIBRARY_PATH & PYTHONPATH before 
-        # launching Softimage.  Note, these can be overriden by specifying a location earlier 
+
+        # On Linux, Softimage 2013 is missing libssl and sqlite3 libraries.  We have some that
+        # we think will work so lets _append_ them to the LD_LIBRARY_PATH & PYTHONPATH before
+        # launching Softimage.  Note, these can be overriden by specifying a location earlier
         # in the LD_LIBRARY_PATH & PYTHONPATH if needed
         if sys.platform == "linux2":
-            # Note: we can't reliably check the version as the path on linux 
+            # Note: we can't reliably check the version as the path on linux
             # is typically just 'xsi'.  This may become a problem if we start
             # to support 2014 and beyond...
             #
@@ -393,7 +401,7 @@ class LaunchApplication(tank.platform.Application):
             app_args = "%s %s" % (app_args, new_args)
         else:
             app_args = new_args
-            
+
         return app_args
 
     def prepare_3dsmax_launch(self, app_args):
@@ -401,18 +409,18 @@ class LaunchApplication(tank.platform.Application):
         3DSMax specific pre-launch environment setup.
 
         Make sure launch args include a maxscript to load the python engine:
-        3dsmax.exe somefile.max -U MAXScript somescript.ms        
+        3dsmax.exe somefile.max -U MAXScript somescript.ms
         """
         startup_dir = os.path.abspath(os.path.join(self._get_app_specific_path("3dsmax"), "startup"))
         os.environ["TANK_BOOTSTRAP_SCRIPT"] = os.path.join(startup_dir, "tank_startup.py")
-        
+
         new_args = "-U MAXScript \"%s\"" % os.path.join(startup_dir, "init_tank.ms")
 
         if app_args:
             app_args = "%s %s" % (new_args, app_args)
         else:
             app_args = new_args
-            
+
         return app_args
 
 
@@ -517,7 +525,7 @@ class LaunchApplication(tank.platform.Application):
         # Store data needed for bootstrapping Tank in env vars. Used in startup/menu.py
         os.environ["TANK_PHOTOSHOP_PYTHON"] = python_path
         os.environ["TANK_PHOTOSHOP_BOOTSTRAP"] = os.path.join(engine_path, "bootstrap", "engine_bootstrap.py")
-        
+
         # unused values, but the photoshop engine code still looks for these...
         os.environ["TANK_PHOTOSHOP_ENGINE"] = "dummy_value"
         os.environ["TANK_PHOTOSHOP_PROJECT_ROOT"] = "dummy_value"
