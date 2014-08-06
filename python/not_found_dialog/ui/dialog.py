4741ec99c28bb5223fcc1f427a490a861411defb
Manne Öhrström <manne@shotgunsoftware.com>
2014-08-06 09:09:17 +0100

    #26089 Added better interaction when an application is not found.
    This implements better feedback when an application is not launched
    corretly. For the shotgun engine, it displays a native error popup with
    embedded html links to documentation. For engines with a UI, it pops up
    a dialog with a button to jump to documentation. For the non-ui case,
    the copy has been improved. This will improve general onramp and the
    desktop workflows in particular. 
    
    Closes #11

---
+++ python/not_found_dialog/ui/dialog.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'dialog.ui'
+#
+#      by: pyside-uic 0.2.13 running on PySide 1.1.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from tank.platform.qt import QtCore, QtGui
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(456, 175)
+        self.verticalLayout = QtGui.QVBoxLayout(Dialog)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout.setSpacing(12)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.logo_example = QtGui.QLabel(Dialog)
+        self.logo_example.setText("")
+        self.logo_example.setPixmap(QtGui.QPixmap(":/res/sg_logo.png"))
+        self.logo_example.setObjectName("logo_example")
+        self.horizontalLayout.addWidget(self.logo_example)
+        self.message = QtGui.QLabel(Dialog)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.message.sizePolicy().hasHeightForWidth())
+        self.message.setSizePolicy(sizePolicy)
+        self.message.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignVCenter)
+        self.message.setWordWrap(True)
+        self.message.setObjectName("message")
+        self.horizontalLayout.addWidget(self.message)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.horizontalLayout_2 = QtGui.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        spacerItem = QtGui.QSpacerItem(17, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.horizontalLayout_2.addItem(spacerItem)
+        self.learn_more = QtGui.QPushButton(Dialog)
+        self.learn_more.setObjectName("learn_more")
+        self.horizontalLayout_2.addWidget(self.learn_more)
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        self.message.setText(QtGui.QApplication.translate("Dialog", "Text text", None, QtGui.QApplication.UnicodeUTF8))
+        self.learn_more.setText(QtGui.QApplication.translate("Dialog", "Learn More", None, QtGui.QApplication.UnicodeUTF8))
+
+from . import resources_rc
