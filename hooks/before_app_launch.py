789de0bd40744df7e4f98ccd62ecfa597482d84b
KP <kp@shotgunsoftware.com>
2015-06-16 16:04:31 -0500

    #29975 add app_path, app_args, version params to before_app_launch hook The before_app_launch hook now gets the same parameters as the app_launch hook which means you have access to the path of the application being launched and any arguments passed in. Also added a version parameter which will contain the version being launched *if* it was defined in the "versions" setting in the Launcher instance (otherwise this will be None).

--- hooks/before_app_launch.py
+++ hooks/before_app_launch.py
@@ -24,9 +24,15 @@ class BeforeAppLaunch(tank.Hook):
     Hook to set up the system prior to app launch.
     """
     
-    def execute(self, **kwargs):
+    def execute(self, app_path, app_args, version, **kwargs):
         """
-        The execute functon of the hook will be called to start the required application        
+        The execute functon of the hook will be called prior to starting the required application        
+        
+        :param app_path: (str) The path of the application executable
+        :param app_args: (str) Any arguments the application may require
+        :param version: (str) version of the application being run if set in the "versions" settings
+                              of the Launcher instance, otherwise None
+
         """
 
         # accessing the current context (current shot, etc)
