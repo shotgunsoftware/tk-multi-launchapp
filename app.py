3c499d997764705fcc2ef5d816fd940f9709ea7b
Jeff Beeland <jeff.beeland@autodesk.com>
2016-01-29 13:54:58 -0800

    #34557 Cleans up the bootstrap module after launch.
    If the "generic" bootstrap routine is used, it will now clean up after itself by removing the bootstrap module that was loaded from sys.modules. This will allow subsequent bootstrap routines that use a different bootstrap module to function properly.
    
    Closes #27.

--- app.py
+++ app.py
@@ -520,8 +520,15 @@ class LaunchApplication(tank.platform.Application):
             self.log_exception("Error executing engine bootstrap script.")
             raise TankError("Error executing bootstrap script. Please see log for details.")
         finally:
-            # remove bootstrap from sys.path
+            # Remove bootstrap from sys.path
             sys.path.pop(0)
+
+            # We also need to unload the bootstrap module so that any
+            # subsequent launches that import a different bootstrap
+            # will succeed.
+            if "bootstrap" in sys.modules:
+                self.log_debug("Removing 'bootstrap' from sys.modules.")
+                del sys.modules["bootstrap"]
 
         return (app_path, new_args)
 
