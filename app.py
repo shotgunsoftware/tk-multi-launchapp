3610db1dd25060c7ad12dd9dc2712cba51e6992f
Amy Hronek <amy.hronek@autodesk.com>
2016-11-16 15:32:45 -0800

    For #38944, Moved HELP_DOC_URL back to app, code cleanup
    Moved HELP_DOC_URL back to app from base_launcher so both BaseLauncher and AppDialog have access to it.

--- app.py
+++ app.py
@@ -20,6 +20,9 @@ class LaunchApplication(sgtk.platform.Application):
     Multi App to launch applications.
     """
 
+    # documentation explaining how to reconfigure app paths
+    HELP_DOC_URL = "https://support.shotgunsoftware.com/entries/95443887#Setting%20up%20Application%20Paths"
+
     def init_app(self):
         """
         Called as app is being initialized
@@ -33,23 +36,23 @@ class LaunchApplication(sgtk.platform.Application):
         else:
             # For traditional setups
             self._launcher = app_payload.SingleConfigLauncher()
-    
+
         # Register the appropriate DCC launch commands
         self._launcher.register_launch_commands()
 
     def launch_from_path_and_context(self, path, context, version=None):
         """
         Launch an app with the specified path and context. The context can
-        contain more information than is available from the path itself, 
+        contain more information than is available from the path itself,
         such as Task information.
         """
         self._launcher.launch_from_path_and_context(path, context, version)
 
     def launch_from_path(self, path, version=None):
         """
-        Launch an app to open the specified file path. Also derive the 
-        context from this path. Note that there are no checks that the 
-        input path is actually compatible with the app that is being launched. 
+        Launch an app to open the specified file path. Also derive the
+        context from this path. Note that there are no checks that the
+        input path is actually compatible with the app that is being launched.
         This should be handled in logic which is external to this app.
         """
         self._launcher.launch_from_path(path, version)
