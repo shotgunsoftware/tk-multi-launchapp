a83d7208a136f183ed2c3f78fc4351fc23b3d966
Eduardo Chauca <eduardo.chauca@autodesk.com>
2024-04-25 09:40:32 -0500

    black format for dialog.py

--- python/not_found_dialog/ui/dialog.py
+++ python/not_found_dialog/ui/dialog.py
@@ -8,6 +8,7 @@
 
 from tank.platform.qt import QtCore, QtGui
 
+
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
@@ -23,19 +24,25 @@ class Ui_Dialog(object):
         self.logo_example.setObjectName("logo_example")
         self.horizontalLayout.addWidget(self.logo_example)
         self.message = QtGui.QLabel(Dialog)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        sizePolicy = QtGui.QSizePolicy(
+            QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding
+        )
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.message.sizePolicy().hasHeightForWidth())
         self.message.setSizePolicy(sizePolicy)
-        self.message.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignVCenter)
+        self.message.setAlignment(
+            QtCore.Qt.AlignLeading | QtCore.Qt.AlignLeft | QtCore.Qt.AlignVCenter
+        )
         self.message.setWordWrap(True)
         self.message.setObjectName("message")
         self.horizontalLayout.addWidget(self.message)
         self.verticalLayout.addLayout(self.horizontalLayout)
         self.horizontalLayout_2 = QtGui.QHBoxLayout()
         self.horizontalLayout_2.setObjectName("horizontalLayout_2")
-        spacerItem = QtGui.QSpacerItem(17, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        spacerItem = QtGui.QSpacerItem(
+            17, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum
+        )
         self.horizontalLayout_2.addItem(spacerItem)
         self.learn_more = QtGui.QPushButton(Dialog)
         self.learn_more.setObjectName("learn_more")
@@ -46,7 +53,16 @@ class Ui_Dialog(object):
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
     def retranslateUi(self, Dialog):
-        self.message.setText(QtGui.QApplication.translate("Dialog", "Text text", None, QtGui.QApplication.UnicodeUTF8))
-        self.learn_more.setText(QtGui.QApplication.translate("Dialog", "Learn More", None, QtGui.QApplication.UnicodeUTF8))
+        self.message.setText(
+            QtGui.QApplication.translate(
+                "Dialog", "Text text", None, QtGui.QApplication.UnicodeUTF8
+            )
+        )
+        self.learn_more.setText(
+            QtGui.QApplication.translate(
+                "Dialog", "Learn More", None, QtGui.QApplication.UnicodeUTF8
+            )
+        )
+
 
 from . import resources_rc
