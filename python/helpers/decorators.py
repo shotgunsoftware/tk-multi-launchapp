05346adf2633e48f7babbab4e60ccc9dc27963ec
Eduardo Chauca <eduardo.chauca@autodesk.com>
2024-04-25 10:23:28 -0500

    docstring added to decorator func

--- python/helpers/decorators.py
+++ python/helpers/decorators.py
@@ -2,6 +2,15 @@ import html
 
 
 def escape_html(field_name):
+    """
+    Decorator that escapes HTML special characters from a
+    specific argument before calling the decorated function.
+
+    :param field_name: Str with message.
+
+    :returns: decorated function.
+    """
+
     def decorator(func):
         def wrapper(*args, **kwargs):
             processed_args = list(args)
