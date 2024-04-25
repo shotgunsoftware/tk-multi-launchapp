97f707fec3ab896bc05dc2ed356bf132c4ae0049
Eduardo Chauca <eduardo.chauca@autodesk.com>
2024-04-25 09:35:11 -0500

    black formatting for new file

--- python/helpers/decorators.py
+++ python/helpers/decorators.py
@@ -8,5 +8,7 @@ def escape_html(field_name):
             if field_name in kwargs:
                 kwargs[field_name] = html.escape(kwargs[field_name])
             return func(*processed_args, **kwargs)
+
         return wrapper
+
     return decorator
