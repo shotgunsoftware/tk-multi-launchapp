67a2bc8adf91d2e68cf915260672478a2e69be92
Carlos Villavicencio <carlos.villavicencio@autodesk.com>
2025-08-12 11:37:04 -0500

    Use import names

--- python/tk_multi_launchapp/base_launcher.py
+++ python/tk_multi_launchapp/base_launcher.py
@@ -15,15 +15,18 @@ import sys
 LooseVersion = None
 try:
     from packaging import version
+    version_parse = version.parse
 except ModuleNotFoundError:
     try:
         from setuptools._distutils.version import LooseVersion
+        version_parse = LooseVersion
     except ModuleNotFoundError:
         try:
             from distutils.version import LooseVersion
+            version_parse = LooseVersion
         except ModuleNotFoundError:
             # Fall back to string comparison
-            pass
+            version_parse = str
 
 import sgtk
 import sgtk.util
@@ -494,19 +497,6 @@ class BaseLauncher(object):
         """
         raise NotImplementedError
 
-    def _version_parse(self, version_string):
-        """
-        Returns a version object that can be used to sort arbitrary version numbers.
-
-        For environment running Python 3.11 or prior it uses the LooseVersion class
-        from distutils.version. For Python 3.12 and later, it uses the
-        version.parse function from the packaging module.
-        """
-        if LooseVersion:
-            return LooseVersion(version_string)
-
-        return version.parse(version_string)
-
     def _sort_versions(self, versions):
         """
         Uses standard python modules to determine how to sort arbitrary version numbers.
@@ -531,7 +521,7 @@ class BaseLauncher(object):
         with ctx_mgr():
             # Cast the incoming version strings to sort them
             try:
-                sort_versions = [self._version_parse(v) for v in versions]
+                sort_versions = [version_parse(v) for v in versions]
             except Exception as e:
                 sort_versions = [get_clean_version_string(v) for v in versions]
 
