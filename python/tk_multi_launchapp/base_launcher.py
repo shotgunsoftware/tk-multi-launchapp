3f8829633c786229aa050c565c8d0b88ce803caa
Carlos Villavicencio <carlos.villavicencio@autodesk.com>
2025-10-28 14:32:24 -0500

    Fix style

--- python/tk_multi_launchapp/base_launcher.py
+++ python/tk_multi_launchapp/base_launcher.py
@@ -288,7 +288,9 @@ class BaseLauncher(object):
                 # Write an event log entry
                 self._register_event_log(menu_name, app_engine, context, launch_cmd)
                 # got UI support. Launch dialog with nice message unless disabled in configuration.
-                if self._tk_app.engine.has_ui and self._tk_app.get_setting("show_launch_indicator"):
+                if self._tk_app.engine.has_ui and self._tk_app.get_setting(
+                    "show_launch_indicator"
+                ):
                     self.launch_indicator(app_path)
 
         except Exception as launch_app_error:
