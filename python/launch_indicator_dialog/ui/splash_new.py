b0ec49a0a61ddc7b6d3f4074e2e22929c7619efb
Eduardo Chauca <166560435+eduardoChaucaGallegos@users.noreply.github.com>
2026-01-29 11:05:59 -0500

    SG-37555 Removed old build_resources.sh (#108)

--- python/launch_indicator_dialog/ui/splash_new.py
+++ python/launch_indicator_dialog/ui/splash_new.py
@@ -1,59 +1,84 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'splash_new.ui'
-#
-#      by: pyside-uic 0.2.15 running on PySide 1.2.2
-#
-# WARNING! All changes made in this file will be lost!
+################################################################################
+## Form generated from reading UI file 'splash_new.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
 
-from tank.platform.qt import QtCore, QtGui
+from tank.platform.qt import QtCore
+for name, cls in QtCore.__dict__.items():
+    if isinstance(cls, type): globals()[name] = cls
+
+from tank.platform.qt import QtGui
+for name, cls in QtGui.__dict__.items():
+    if isinstance(cls, type): globals()[name] = cls
+
+
+from ..qtwidgets import ShotgunSpinningWidget
+
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
+        if not Dialog.objectName():
+            Dialog.setObjectName(u"Dialog")
         Dialog.resize(446, 156)
-        Dialog.setStyleSheet("background-color: rgb(41, 41, 41);")
-        self.verticalLayout = QtGui.QVBoxLayout(Dialog)
-        self.verticalLayout.setObjectName("verticalLayout")
-        self.verticalLayout_2 = QtGui.QVBoxLayout()
-        self.verticalLayout_2.setObjectName("verticalLayout_2")
-        spacerItem = QtGui.QSpacerItem(20, 11, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Fixed)
-        self.verticalLayout_2.addItem(spacerItem)
-        self.horizontalLayout = QtGui.QHBoxLayout()
+        Dialog.setStyleSheet(u"background-color: rgb(41, 41, 41);")
+        self.verticalLayout = QVBoxLayout(Dialog)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout_2 = QVBoxLayout()
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.verticalSpacer = QSpacerItem(20, 11, QSizePolicy.Minimum, QSizePolicy.Fixed)
+
+        self.verticalLayout_2.addItem(self.verticalSpacer)
+
+        self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setSpacing(12)
-        self.horizontalLayout.setObjectName("horizontalLayout")
-        spacerItem1 = QtGui.QSpacerItem(128, 20, QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Minimum)
-        self.horizontalLayout.addItem(spacerItem1)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalSpacer_2 = QSpacerItem(128, 20, QSizePolicy.Fixed, QSizePolicy.Minimum)
+
+        self.horizontalLayout.addItem(self.horizontalSpacer_2)
+
         self.sg_spinning_wid = ShotgunSpinningWidget(Dialog)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Fixed)
+        self.sg_spinning_wid.setObjectName(u"sg_spinning_wid")
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.sg_spinning_wid.sizePolicy().hasHeightForWidth())
         self.sg_spinning_wid.setSizePolicy(sizePolicy)
-        self.sg_spinning_wid.setMinimumSize(QtCore.QSize(0, 83))
-        self.sg_spinning_wid.setObjectName("sg_spinning_wid")
+        self.sg_spinning_wid.setMinimumSize(QSize(0, 83))
+
         self.horizontalLayout.addWidget(self.sg_spinning_wid)
-        spacerItem2 = QtGui.QSpacerItem(128, 20, QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Minimum)
-        self.horizontalLayout.addItem(spacerItem2)
+
+        self.horizontalSpacer = QSpacerItem(128, 20, QSizePolicy.Fixed, QSizePolicy.Minimum)
+
+        self.horizontalLayout.addItem(self.horizontalSpacer)
+
         self.verticalLayout_2.addLayout(self.horizontalLayout)
-        self.message = QtGui.QLabel(Dialog)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Maximum)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.message.sizePolicy().hasHeightForWidth())
-        self.message.setSizePolicy(sizePolicy)
-        self.message.setMaximumSize(QtCore.QSize(16777215, 18))
-        self.message.setText("")
-        self.message.setAlignment(QtCore.Qt.AlignHCenter|QtCore.Qt.AlignTop)
+
+        self.message = QLabel(Dialog)
+        self.message.setObjectName(u"message")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Maximum)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.message.sizePolicy().hasHeightForWidth())
+        self.message.setSizePolicy(sizePolicy1)
+        self.message.setMaximumSize(QSize(16777215, 18))
+        self.message.setAlignment(Qt.AlignHCenter|Qt.AlignTop)
         self.message.setWordWrap(True)
-        self.message.setObjectName("message")
+
         self.verticalLayout_2.addWidget(self.message)
+
         self.verticalLayout.addLayout(self.verticalLayout_2)
 
         self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+        QMetaObject.connectSlotsByName(Dialog)
+    # setupUi
 
     def retranslateUi(self, Dialog):
+        self.message.setText("")
         pass
-
-from ..qtwidgets import ShotgunSpinningWidget
+    # retranslateUi
